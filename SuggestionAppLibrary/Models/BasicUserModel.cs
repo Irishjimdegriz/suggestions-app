fece4e8a9b75017229c9681c054a00fd1434ccdd
Eugene Zaznoba <irishjimdegriz@gmail.com>
2022-05-14 18:28:39 +0300

    Init Suggestions App

---
+++ SuggestionAppLibrary/Models/BasicUserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SuggestionAppLibrary.Models;
+public class BasicUserModel
+{
+    [BsonId]
+    public string Id { get; set; }
+    public string DisplayName { get; set; }
+
+    public BasicUserModel()
+    {
+
+    }
+
+    public BasicUserModel(UserModel user)
+    {
+        Id = user.Id;
+        DisplayName = user.DisplayName;
+    }
+}
